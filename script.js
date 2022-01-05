97ae2e76654cd56c03a3bc2a85243a408af9dbb9
Hirad Fazeli <82698542+hiradfazeli@users.noreply.github.com>
2022-01-05 17:47:01 +0400

    Messages stored

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const message = {
     },
 
     contentFill(type, content){
-        type.toLoweCase();
+        type.toLowerCase();
         if(type === 'joke'){
             return this.jokes.content.push(content);
         }else if(type === 'quote'){
@@ -28,4 +28,15 @@ const message = {
     }
 }
 
-console.log(message.ads.type);
+message.contentFill('joke', `Did you hear about the mathematician who’s afraid of negative numbers?\nHe’ll stop at nothing to avoid them.`);
+message.contentFill('joke', `What’s the best thing about Switzerland?\nI don’t know, but the flag is a big plus.`);
+message.contentFill('joke', `I invented a new word!\nPlagiarism!`);
+message.contentFill('quote', `Great things in business are never done by one person. They're done by a team of people.`);
+message.contentFill('quote', `Design is not just what it looks like and feels like. Design is how it works.`);
+message.contentFill('quote', `Stay hungry, stay foolish.`);
+message.contentFill('advertisement', `Privacy. That's iPhone.`);
+message.contentFill('advertisement', `It doesn't take a genius.`);
+message.contentFill('advertisement', `Brilliant. In every way.`);
+
+
+console.log(message.jokes.content);
